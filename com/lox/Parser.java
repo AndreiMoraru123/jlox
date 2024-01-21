4d81adef0ccf92bd06aafcb2ee0d94229683f163
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2024-01-21 16:55:48 +0200

    generating AST

--- com/lox/Parser.java
+++ com/lox/Parser.java
@@ -1,2 +1,14 @@
-package com.lox;public class Parser {
+package com.lox;
+
+import java.util.List;
+import static com.lox.TokenType.*;
+
+public class Parser {
+    private final List<Token> tokens;
+    private int current = 0;
+
+    public Parser(List<Token> tokens) {
+        this.tokens = tokens;
+    }
+
 }
